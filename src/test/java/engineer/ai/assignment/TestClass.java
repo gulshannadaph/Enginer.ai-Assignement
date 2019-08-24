54b0b93175acd8b7c607e8ff9b1ad2f93686dcf6
Gulshan Nadaph <gulshanaranadaph@gmail.com>
2019-08-24 17:05:36 +0530

    finding element

--- src/test/java/engineer/ai/assignment/TestClass.java
+++ src/test/java/engineer/ai/assignment/TestClass.java
@@ -1,5 +1,40 @@
 package engineer.ai.assignment;
 
-public class TestClass {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
+public class TestClass {
+	public WebDriver driver;
+	
+	@Test
+	public void test_Hover() throws InterruptedException{
+	
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.rrrgggbbb.com/");
+		driver.manage().window().maximize();
+		
+		WebElement rfill = driver.findElement(By.id("R-fill"));
+		WebElement gfill = driver.findElement(By.id("G-fill"));
+		WebElement bfill = driver.findElement(By.id("B-fill"));
+		
+		Thread.sleep(3000);
+		
+		Actions action = new Actions(driver);
+		action.moveToElement(rfill).build().perform();
+		
+		
+		
+		
+		
+		
+		
+	}
+	
 }
